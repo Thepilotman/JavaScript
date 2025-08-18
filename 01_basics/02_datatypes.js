0640413d1b8e5985b15d62593dfb517cebe43d4d
Thepilotman <chintanshetty72@gmail.com>
2025-08-18 18:21:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+
+console.log("Chintan")
+
+
+let name = "Chintan"
+let age  = 18
+let isLoggedIn = true
+let temperature  = null
+
+// number => 2 to power 53
+// bigINt
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol  => unique
+
+
+//object
+
+
+console.log(typeof null)
